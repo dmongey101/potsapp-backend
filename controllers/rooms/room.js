47dfc2b357d0743365e3e8177c0381748434167c
Sergio Zullo <sergio.zullo@tekenable.com>
2020-05-04 22:19:40 +0100

    Assigning teams

--- controllers/rooms/room.js
+++ controllers/rooms/room.js
@@ -18,27 +18,50 @@ exports.createRoom = (req, res) => {
 
     const player = {
         name: req.body.playerName,
-        team: req.body.team,
         room: req.body.roomName
     }
+
+    const noOfTeams = parseInt(req.body.noOfTeams);
 
     rooms[req.body.roomName] = {
         noOfPlayers: req.body.noOfPlayers,
         noOfTeams: req.body.noOfTeams,
         players: [player],
         pot: [],
-        pot2: []
+        pot2: [],
+        teams: {}
     }
+
+    for (var i = 1; i <= noOfTeams; i++) {
+        rooms[req.body.roomName].teams[i.toString()] = [];
+    }
+
+    assignTeam(player, noOfTeams)
+
+    console.log(rooms);
 }
 
 exports.joinRoom = (req, res) => {
-    var team = Math.floor(Math.random() * parseInt(rooms[req.body.roomName].noOfPlayers))
+
     const player = {
         name: req.body.playerName,
-        team: team,
         room: req.body.roomName
     }
-    rooms[req.body.roomName].players.push(player)
+    rooms[req.body.roomName].players.push(player);
+
+    assignTeam(player);
+}
+
+function assignTeam(player) {
+    const noOfPlayers = parseInt(rooms[player.room].noOfPlayers);
+    const noOfTeams = parseInt(rooms[player.room].noOfTeams);
+
+    for (var i = 1; i <= noOfPlayers; i++) {
+        if (rooms[player.room].teams[i.toString()].length <= (noOfPlayers/noOfTeams)) {
+            rooms[player.room].teams[i.toString()].push(player);
+            break;
+        }
+    }
 }
 
 exports.rooms = rooms
